a32745488dc11859441ed82d87a3d7f792e832e8
rninomiya <risakonino@yahoo.co.jp>
2014-10-30 19:36:21 +0900

    Sensenote

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,27 @@
+class NotesController < ApplicationController
+  def new
+    @note = Note.new
+  end
+
+ def create
+    @note = Note.new(note_params)
+    if @note.save 
+      redirect_to @note
+    else
+      render :new
+    end
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body, :date)
+  end
+
+ def show
+    @note = Note.find(params[:id])
+ end
+
+ def index
+    @notes = Note.all
+ end
+
+end
